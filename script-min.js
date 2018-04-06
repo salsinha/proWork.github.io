0d2f5b76590c19d84f5a856dfd68e735d39228e4
Alexandre Salsinha <alexsalsinha@gmail.com>
2018-04-06 11:22:30 +0100

    Commit proWork

---
+++ script-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#hmb-nav-icon").click(function(){$(".bars").toggleClass("close"),$(".hmb-nav").toggleClass("close")})});
---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("#hmb-nav-icon").click(function () {
+        $(".bars").toggleClass("close");
+        $(".hmb-nav").toggleClass("close");
+    });
+});
